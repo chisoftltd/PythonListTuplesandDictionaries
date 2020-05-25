de5ffbcf11d55dc7aad772620a38fdcfd07669c1
chisoftltd <chisoftltd@gmail.com>
2020-05-26 00:27:56 +0200

    Simple List Operations Indexing, Slicing, and Matrices Python List Functions Python List Methods

--- PythonListTuplesandDictionaries.py
+++ PythonListTuplesandDictionaries.py
@@ -28,3 +28,32 @@ print(names)
 
 names.remove("Nnaedozie")
 print(names)
+
+# Simple List Operations
+print(scores + names)
+print(names[2:5])
+print(scores * 3)
+print("Chinwe" in names)
+
+for x in names:
+    print(x)
+
+# Indexing, Slicing, and Matrices
+
+print(names[2:])
+print(names[-2])
+print(names[4])
+
+# Python List Functions
+
+print(max(grades))
+print(min(scores))
+print(max(names))
+
+# Python List Methods
+print(names.append("Bonaventure"))
+print(names)
+print(grades.extend(scores))
+print(scores.reverse())
+print(scores)
+print(grades)
